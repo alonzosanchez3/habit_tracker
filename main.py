6bbb3afefc4d2853e71533e74c0cdadbdfeddc02
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-25 12:12:00 -0600

    created account

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+USERNAME = 'alonzo123'
+TOKEN = 'hkl123456'
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+  "token": TOKEN,
+  "username": USERNAME,
+  "agreeTermsOfService": "yes",
+  "notMinor": 'yes'
+}
+
+# response = requests.post(pixela_endpoint, json=user_params)
+# print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+  "id": 'graph1',
+  "name": 'Coding Graph',
+  "unit": 'Day',
+  "type": 'int',
+  "color": 'ajisai'
+}
+
+
+response = requests.post(graph_endpoint, json=graph_config)
